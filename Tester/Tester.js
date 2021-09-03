276193db3de381e014d629b3cf1c375111a51555
JonathanRut <jmrutland@gmail.com>
2021-09-03 20:20:38 +0100

    Added ship and sqaure overlay

--- Tester/Tester.js
+++ Tester/Tester.js
@@ -10,27 +10,28 @@ class Tester extends Phaser.Scene{
 
     create(){
         //plotting the grid
-        const gridx = 86;
+        const gridx = 116;
         const gridy = 56;
+        let grid = [];
         for(let i = 0; i<10;i++){
+            grid.push([]);
             for(let j = 0; j < 10; j++){
-                const rectangle = this.add.rectangle(gridx+i*30,gridy+j*30,30,30,0xffffff).setOrigin(0,0);
+                const rectangle = this.add.rectangle(gridx+j*30,gridy+i*30,30,30,0xffffff).setOrigin(0,0);
                 rectangle.setStrokeStyle(2,0x000000);
                 //creating the functionality of the squares
                 rectangle.setInteractive();
-                rectangle.on('pointerover',function(){
-                    rectangle.setFillStyle(0xd0d0d0);
-                });
-                rectangle.on('pointerout',function()
-                {
-                    rectangle.setFillStyle(0xffffff);
-                });
-                rectangle.on('pointerdown',function()
-                {
-                    this.add.circle(rectangle.x+10,rectangle.y+10,10,0xff0000).setStrokeStyle(1,0x000000).setOrigin(0,0);
-                },this);
+                //rectangle.on('pointerover',function(){
+                //    rectangle.setFillStyle(0xd0d0d0);
+                //});
+                //rectangle.on('pointerout',function()
+                //{
+                //    rectangle.setFillStyle(0xffffff);
+                //});
+                grid[i].push(rectangle);
             }
         }
+
+
         for(let i = 0; i<10; i++){
             this.add.text(gridx+1,i*30+gridy+15,String(i+1), {fontFamily:'Arial' ,fontSize:'12px', fill:'#000000'});
         }
@@ -39,18 +40,23 @@ class Tester extends Phaser.Scene{
             this.add.text(i*30+gridx+22,gridy-1,letters[i], {fontFamily:'Arial' ,fontSize:'12px', fill:'#000000'});
         }
 
-
-        //for(let i = 0; i < 4; i++){
-        //    this.add.circle(400,270 - i * 30,10,0xff0000).setStrokeStyle(1,0x000000);
-        //}
-        
         const battleship = this.add.sprite(90,60,'battleship');
         battleship.setOrigin(0,0);
+
         battleship.setInteractive();
         this.input.setDraggable(battleship);
+
         this.input.on('drag',function(pointer,target,dragX,dragY){
             target.x = dragX - dragX % 30;
             target.y = dragY - dragY % 30;
+            for(let i = 0; i < target.height/27-(target.height%27)/27; i++){
+                grid[((target.y - gridy - 4)/30) + i][(target.x - gridx - 4)/30].setFillStyle(0xd0d0d0);
+                console.log((target.x - gridx - 4)/30);
+                //console.log(grid[(target.y - gridy)/30 + i][(target.x - gridx)/30]);
+                
+            }
+            console.log("break");
         });
+        
     }
 }
