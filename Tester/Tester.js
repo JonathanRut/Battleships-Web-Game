49e0b423d85203d75ede03f6e49f4e6f49a9b138
JonathanRut <jmrutland@gmail.com>
2021-10-14 12:23:19 +0100

    Adding new ship

--- Tester/Tester.js
+++ Tester/Tester.js
@@ -23,6 +23,7 @@ class Tester extends Phaser.Scene{
             for(let j = 0; j < 10; j++){
                 const rectangle = this.add.rectangle(this.gridx+j*30,this.gridy+i*30,30,30,0xffffff).setOrigin(0,0);
                 rectangle.setStrokeStyle(2,0x000000);
+                rectangle.status = 'empty'
                 this.grid[i].push(rectangle);
             }
         }
@@ -36,65 +37,43 @@ class Tester extends Phaser.Scene{
             this.add.text(i*30+this.gridx+22,this.gridy-1,letters[i], {fontFamily:'Arial' ,fontSize:'12px', fill:'#000000'});
         }
 
+
+
         this.battleship = this.add.sprite(90,60,'battleship-ver');
         this.battleship.setOrigin(0,0);
         this.carrier = this.add.sprite(60,60,'carrier-ver');
         this.carrier.setOrigin(0,0);
 
-        //battleship.angle += 90;
-        //let temp = battleship.height;
-        //battleship.height = battleship.width;
-        //battleship.width = temp;
-
         //Only vertical ships??
-
         
         this.battleship.setInteractive();
         this.input.setDraggable(this.battleship, true);
         this.carrier.setInteractive();
         this.input.setDraggable(this.carrier, true);
+        this.battleship.on('pointerup',function(){this.flipShip(this.battleship,'battleship-hor','battleship-ver')},this);
+        this.carrier.on('pointerup',function(){this.flipShip(this.carrier,'carrier-hor','carrier-ver')},this);        
 
 
         this.input.on('drag',function(pointer,target,dragX,dragY){
             if(target.x !== dragX - dragX % 30 || target.y !== dragY - dragY % 30){
-                this.updateOverlayColour(target,0xffffff);
+                this.leaveSquare(target);
                 target.x = dragX - dragX % 30;
                 target.y = dragY - dragY % 30;
-                this.updateOverlayColour(target,0xd0d0d0);
+                this.enterSquare(target);
                 target.justDragged = true;
             }
         },this);
-
-        // this.battleship.flipShip = function(){
-        //     if(this.battleship.justDragged){
-        //         this.battleship.justDragged = false;
-        //     }
-        //     else{
-        //         //Need to change hit area manually
-
-        //         this.updateOverlayColour(this.battleship,0xffffff);
-        //         if(this.battleship.texture.key === 'battleship-ver'){
-        //             this.battleship.setTexture('battleship-hor');
-        //         }
-        //         else{
-        //             this.battleship.setTexture('battleship-ver');
-        //         }
-        //         let temp = this.battleship.input.hitArea.height;
-        //         this.battleship.input.hitArea.height = this.battleship.input.hitArea.width;
-        //         this.battleship.input.hitArea.width = temp;
-        //         this.updateOverlayColour(this.battleship,0xd0d0d0);
-        //     }
-        // }
-
-        this.battleship.on('pointerup',function(){this.flipShip(this.battleship,'battleship-hor','battleship-ver')},this);
-        this.carrier.on('pointerup',function(){this.flipShip(this.carrier,'carrier-hor','carrier-ver')},this);        
     }
 
-    updateOverlayColour(target,colour){
-        for(let i = 0; i < Math.round(target.height/27); i++){
-            for(let j = 0; j < Math.round(target.width/27); j++){
+
+
+    leaveSquare(target){
+        for(let i = 0; i < Math.round(target.height/27 + 2); i++){
+            for(let j = 0; j < Math.round(target.width/27 + 2); j++){
                 try{
-                    this.grid[((target.y - this.gridy - 4)/30) + i][((target.x - this.gridx - 4)/30) + j].setFillStyle(colour);
+                    let rectange = this.grid[((target.y - this.gridy - 4)/30) + i - 1][((target.x - this.gridx - 4)/30) + j - 1];
+                    rectange.setFillStyle(0xffffff);
+                    rectange.status = 'empty'
                 }
                 catch{
                     continue;
@@ -103,14 +82,43 @@ class Tester extends Phaser.Scene{
         }
     }
 
+
+
+    enterSquare(target){
+        for(let i = 0; i < Math.round(target.height/27) + 2; i++){
+            for(let j = 0; j < Math.round(target.width/27) + 2; j++){
+                try{
+                    let rectangle = this.grid[((target.y - this.gridy - 4)/30) + i - 1][((target.x - this.gridx - 4)/30) + j - 1];
+                    rectangle.setFillStyle(0xd0d0d0);
+                    rectangle.status = 'ship-border';
+                }
+                catch{
+                    continue;
+                }
+            }
+        }
+        for(let i = 0; i < Math.round(target.height/27); i++){
+            for(let j = 0; j < Math.round(target.width/27); j++){
+                try{
+                    let rectangle = this.grid[((target.y - this.gridy - 4)/30) + i][((target.x - this.gridx - 4)/30) + j]
+                    rectangle.setFillStyle(0xa0a0a0);
+                    rectangle.status = 'ship'
+                }
+                catch{
+                    continue;
+                }
+            }
+        }
+    }
+
+
+
     flipShip = function(target,horKey,verKey){
         if(target.justDragged){
             target.justDragged = false;
         }
         else{
-            //Need to change hit area manually
-
-            this.updateOverlayColour(target,0xffffff);
+            this.leaveSquare(target);
             if(target.texture.key === verKey){
                 target.setTexture(horKey);
             }
@@ -120,7 +128,7 @@ class Tester extends Phaser.Scene{
             let temp = target.input.hitArea.height;
             target.input.hitArea.height = target.input.hitArea.width;
             target.input.hitArea.width = temp;
-            this.updateOverlayColour(target,0xd0d0d0);
+            this.enterSquare(target);
         }
     }
 }
