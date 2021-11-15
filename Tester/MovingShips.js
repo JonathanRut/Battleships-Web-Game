7503f89acde35275b5fb84052b924a565444c24d
JonathanRut <jmrutland@gmail.com>
2021-11-15 21:33:02 +0000

    Can drag ships in grid with collision

--- Tester/MovingShips.js
+++ Tester/MovingShips.js
@@ -3,12 +3,14 @@ class MovingShips
     shipParts = []   
     rotation = "ver"
 
-    constructor(length, origin, scene)
+    constructor(length, origin, rotation, scene)
     {
         this.scene = scene;
-        let top = this.NewPart({x:90,y:60},2);
-        const plusButton = scene.add.sprite(80,60,'plus').setOrigin(0,0);
-        const minusButton = scene.add.sprite(80,70,'minus').setOrigin(0,0);
+        this.rotation = rotation;
+
+        let top = this.NewPart(origin,2);
+        const plusButton = scene.add.sprite(origin.x - 10, origin.y,'plus').setOrigin(0,0);
+        const minusButton = scene.add.sprite(origin.x - 10, origin.y + 10,'minus').setOrigin(0,0);
         
         plusButton.setInteractive();
         minusButton.setInteractive();
@@ -17,6 +19,12 @@ class MovingShips
         minusButton.on('pointerup',()=>{this.RemoveLength()},this);
         
         this.shipParts.push(plusButton,minusButton,top);
+
+        this.UpdateShipCells(0xa0a0a0,0xd0d0d0);
+        while(this.shipParts.length < length + 2)
+        {
+            this.AddLength();
+        }
     }
 
     NewPart(origin, index)
@@ -36,29 +44,33 @@ class MovingShips
 
     AddLength()
     {
-        if(this.shipParts.length === 12)
-        {
-            return;
-        }
         const lastship = this.shipParts.length - 1;
         const newX = this.shipParts[lastship].x + (this.rotation === "ver" ? 0:30);
         const newY = this.shipParts[lastship].y + (this.rotation === "ver" ? 30:0);
+        if(this.shipParts.length === 12 || (newX - this.scene.gridx - 4)/30 > 9 || (newY - this.scene.gridy - 4)/30 > 9)
+        {
+            return false;
+        }
+        if(this.scene.grid[(newY - this.scene.gridy - 4)/30][(newX - this.scene.gridx - 4)/30].borders.length > 1)
+        {
+            return false;
+        }
         const newPart = this.NewPart({x:newX,y:newY},this.shipParts.length);
         this.shipParts.push(newPart);
-        this.UpdateGridShade(0xa0a0a0,0xd0d0d0,this.shipParts.length - 1);
+        this.UpdateShipCells(0xa0a0a0,0xd0d0d0);
+        return true;
     }
 
     RemoveLength()
     {
         if(this.shipParts.length > 3)
         {
-            this.UpdateGridShade(0xffffff,0xffffff,this.shipParts.length - 1);
+            this.UpdateShipCells(0xffffff,0xffffff);;
             const deletedShip = this.shipParts.pop();
-            for(let i = 2; i < this.shipParts.length; i++)
-            {
-                this.UpdateGridShade(0xa0a0a0,0xd0d0d0,i);
-            }
             deletedShip.destroy();
+            this.scene.ships.forEach(ship => {
+                ship.UpdateShipCells(0xa0a0a0,0xd0d0d0);
+            });
         }
     }
 
@@ -74,88 +86,121 @@ class MovingShips
             {
                 this.RemoveLength();
             }
-            this.rotation = this.rotation === "ver" ? "hor":"ver"
+            this.rotation = this.rotation === "ver" ? "hor":"ver";
 
             for(let i = 0; i < shipLength; i++)
             {
-                this.AddLength();
+               if(!this.AddLength())
+               {
+                    this.Rotate();
+                    this.AddLength();
+               }
             }
+            this.scene.ships.forEach(ship => {
+                ship.UpdateShipCells(0xa0a0a0,0xd0d0d0);
+            });
         }
     }
 
     Drag(dragCoords, index)
     {
         let distances = [];
+        const temp = this.shipParts;
         for(let i = 0; i < this.shipParts.length; i++)
         {
             let xdistance = this.shipParts[i].x - this.shipParts[index].x;
             let ydistance = this.shipParts[i].y - this.shipParts[index].y;
             distances.push({x:xdistance,y:ydistance});
-            i > 1 ? this.UpdateGridShade(0xffffff,0xffffff,i) : null;
         }
+        this.UpdateShipCells(0xffffff,0xffffff);
+
+        for(let i = 2; i < this.shipParts.length; i++)
+        {
+            let newXGridPos = ((dragCoords.x + distances[i].x) - this.scene.gridx - 4)/30;
+            let newYGridPos = ((dragCoords.y + distances[i].y) - this.scene.gridy - 4)/30;
+            if(newXGridPos < 0 || newXGridPos > 9 || newYGridPos < 0 || newYGridPos > 9)
+            {
+                return;
+            }
+            try
+            {
+                if(this.scene.grid[newYGridPos][newXGridPos].borders.length > 0 || this.scene.grid[newYGridPos][newXGridPos].ships.length > 0)
+                {
+                    return;
+                }
+            }catch{continue;}
+            
+        }
+
         for(let i = 0; i < this.shipParts.length; i++)
         {
             this.shipParts[i].x = dragCoords.x + distances[i].x;
             this.shipParts[i].y = dragCoords.y + distances[i].y;
-            i > 1 ? this.UpdateGridShade(0xa0a0a0,0xd0d0d0,i) : null;
         }
     }
 
-    UpdateGridShade(shipColour,borderColour,index)
+    UpdateShipCells(shipColour,borderColour)
     {
-        const gridx = ((this.shipParts[index].x - this.scene.gridx - 4)/30)
-        const gridy = ((this.shipParts[index].y - this.scene.gridy - 4)/30)
-        if(index === 2)
+        const originGridPosX = ((this.shipParts[2].x - this.scene.gridx - 4)/30);
+        const originGridPosY = ((this.shipParts[2].y - this.scene.gridy - 4)/30);
+        for(let i = -1; i < this.shipParts.length - 1; i++)
         {
-            for(let i = -1; i < 2; i++)
-            {
-                for(let j = -1; j < 2; j++)
-                {
-                    try
-                    {
-                        const rectangle =  this.scene.grid[gridy + i][gridx + j]
-                        rectangle.setFillStyle(borderColour);
-                        rectangle.status = 'border';
-                    }
-                    catch{continue;}
-                }
-            }
-        }
-        else if(index === this.shipParts.length-1)
-        {
-            for(let i = 0; i < 2; i++)
-            {
-                for(let j = 0; j < 3; j++)
-                {
-                    try
-                    {
-                        const rectangle =  this.scene.grid[gridy + (this.rotation === "ver" ? i  : j - 1)][gridx + (this.rotation === "ver" ?  j - 1 : i)]
-                        rectangle.setFillStyle(borderColour);
-                        rectangle.status = 'border';
-                    }
-                    catch{continue;}
-                }
-            }
-        }
-        else
-        {
-            for(let i = 0; i < 3; i++)
+            for(let j = -1; j < 2; j++)
             {
                 try
                 {
-                    const rectangle =  this.scene.grid[gridy + (this.rotation === "ver" ?  null : i - 1)][gridx + (this.rotation === "ver" ?  i - 1 : null)]
+                    const rectangle =  this.scene.grid[originGridPosY + (this.rotation === "ver" ? i:j)][originGridPosX +  (this.rotation === "ver" ? j:i)];
                     rectangle.setFillStyle(borderColour);
-                    rectangle.status = 'border';
+                    if (borderColour === 0xffffff)
+                    {
+                        const index = rectangle.borders.indexOf(this)
+                        rectangle.borders.splice(index,1);
+                    }
+                    else if (rectangle.borders.indexOf(this) === -1)
+                    {
+                        rectangle.borders.push(this);
+                    }
                 }
                 catch{continue;}
             }
         }
-        try
+        for(let i = 0; i < this.shipParts.length - 2; i++)
         {
-            const rectangle =  this.scene.grid[gridy][gridx]
-            rectangle.setFillStyle(shipColour);
-            rectangle.status = 'ship';
+            try
+            {
+                const rectangle =  this.scene.grid[originGridPosY + (this.rotation === "ver" ? i:0)][originGridPosX +  (this.rotation === "ver" ? 0:i)];
+                rectangle.setFillStyle(shipColour);
+                if (shipColour === 0xffffff)
+                {
+                    const index = rectangle.ships.indexOf(this)
+                    rectangle.ships.splice(index,1);
+                }
+                else if (rectangle.ships.indexOf(this) === -1)
+                {
+                    rectangle.ships.push(this);
+                }
+                const index = rectangle.borders.indexOf(this);
+                rectangle.borders.splice(index,1);                
+            }
+            catch{continue;}
         }
-        catch{}
+        for(let i = 0; i < 10; i++)
+        {
+            let line = i + "|";
+            for(let j = 0; j < 10; j++)
+            {
+                if(this.scene.grid[i][j].borders.length > 0)
+                {
+                    line += "X|"
+                }
+                else if(this.scene.grid[i][j].ships.length > 0)
+                {
+                    line += "O|"
+                }
+                else{line += " |"}
+            }
+            console.log(line)
+        }
+        console.log("end");
     }
 }
--- Tester/Tester.js
+++ Tester/Tester.js
@@ -8,12 +8,6 @@ class Tester extends Phaser.Scene{
         this.load.image('plus', 'sprites/plus.png');
         this.load.image('minus', 'sprites/minus.png');
         this.load.image('shipPart','sprites/shipPart.png');
-        /*
-        this.load.image('battleship-ver','sprites/Battleship-ver.png');
-        this.load.image('battleship-hor','sprites/Battleship-hor.png');
-        this.load.image('carrier-hor','sprites/Carrier-hor.png');
-        this.load.image('carrier-ver','sprites/Carrier-ver.png')
-        */
     }
 
     create(){
@@ -22,18 +16,16 @@ class Tester extends Phaser.Scene{
         this.gridy = 56;
         this.grid = [];
 
-
         for(let i = 0; i<10;i++){
             this.grid.push([]);
             for(let j = 0; j < 10; j++){
                 const rectangle = this.add.rectangle(this.gridx+j*30,this.gridy+i*30,30,30,0xffffff).setOrigin(0,0);
                 rectangle.setStrokeStyle(2,0x000000);
-                rectangle.status = 'empty'
+                rectangle.borders = [];
+                rectangle.ships = [];
                 this.grid[i].push(rectangle);
             }
         }
-
-
         for(let i = 0; i<10; i++){
             this.add.text(this.gridx+1,i*30+this.gridy+15,String(i+1), {fontFamily:'Arial' ,fontSize:'12px', fill:'#000000'});
         }
@@ -42,81 +34,25 @@ class Tester extends Phaser.Scene{
             this.add.text(i*30+this.gridx+22,this.gridy-1,letters[i], {fontFamily:'Arial' ,fontSize:'12px', fill:'#000000'});
         }
 
-        let battleship = new MovingShips(4,0,this);
+        this.ships = [];
+        
+        const battleship = new MovingShips(4,{x:120,y:60},"ver",this);
+        const carrier = new MovingShips(5,{x:180,y:60},"ver",this);
+        // const cruiser = new MovingShips(3,{x:240,y:60},"ver",this);
+        // const submarine = new MovingShips(3,{x:300,y:60},"ver",this);
+        // const destroyer = new MovingShips(2,{x:360,y:60},"ver",this);
+
+        this.ships.push(battleship,carrier);
 
         this.input.on('drag',function(pointer,target,dragX,dragY){
             if(target.x !== dragX - dragX % 30 || target.y !== dragY - dragY % 30){
                 target.ship.justDragged = true;
                 target.ship.Drag({x:dragX - dragX % 30,y:dragY - dragY % 30},target.index);
+                this.ships.forEach(ship => {
+                    ship.UpdateShipCells(0xa0a0a0,0xd0d0d0);
+                });
             }
         },this);
         
-    }
-
-
-
-    leaveSquare(target){
-        for(let i = 0; i < Math.round(target.height/27 + 2); i++){
-            for(let j = 0; j < Math.round(target.width/27 + 2); j++){
-                try{
-                    const rectange = this.grid[((target.y - this.gridy - 4)/30) + i - 1][((target.x - this.gridx - 4)/30) + j - 1];
-                    rectange.setFillStyle(0xffffff);
-                    rectange.status = 'empty'
-                }
-                catch{
-                    continue;
-                }
-            }
-        }
-    }
-
-
-
-    enterSquare(target){
-        for(let i = 0; i < Math.round(target.height/27) + 2; i++){
-            for(let j = 0; j < Math.round(target.width/27) + 2; j++){
-                try{
-                    let rectangle = this.grid[((target.y - this.gridy - 4)/30) + i - 1][((target.x - this.gridx - 4)/30) + j - 1];
-                    rectangle.setFillStyle(0xd0d0d0);
-                    rectangle.status = 'ship-border';
-                }
-                catch{
-                    continue;
-                }
-            }
-        }
-        for(let i = 0; i < Math.round(target.height/27); i++){
-            for(let j = 0; j < Math.round(target.width/27); j++){
-                try{
-                    let rectangle = this.grid[((target.y - this.gridy - 4)/30) + i][((target.x - this.gridx - 4)/30) + j]
-                    rectangle.setFillStyle(0xa0a0a0);
-                    rectangle.status = 'ship'
-                }
-                catch{
-                    continue;
-                }
-            }
-        }
-    }
-
-
-
-    flipShip = function(target,horKey,verKey){
-        if(target.justDragged){
-            target.justDragged = false;
-        }
-        else{
-            this.leaveSquare(target);
-            if(target.texture.key === verKey){
-                target.setTexture(horKey);
-            }
-            else{
-                target.setTexture(verKey);
-            }
-            let temp = target.input.hitArea.height;
-            target.input.hitArea.height = target.input.hitArea.width;
-            target.input.hitArea.width = temp;
-            this.enterSquare(target);
-        }
     }
 }
